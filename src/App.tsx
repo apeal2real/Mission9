c7202e01954bba263d802dcd100345b5706f6c80
apeal2real <alec.peal@gmail.com>
2024-03-07 21:16:07 -0700

    Mission9 Ready

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,57 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import teamsData from './CollegeBasketballTeams.json';
+
+interface Team {
+  tid: number;
+  cid: number;
+  did: number;
+  school: string;
+  name: string;
+  abbrev: string;
+  pop: number;
+  city: string;
+  state: string;
+  latitude: number;
+  longitude: number;
+}
+
+function Heading() {
+  return <h1>March Madness!</h1>;
+}
+
+class TeamComponent extends React.Component<Team> {
+  render() {
+    const oneTeam = this.props;
+
+    return (
+      <div>
+        <h2>{oneTeam.school}</h2>
+        <h3>Mascot: {oneTeam.name}</h3>
+        <h3>
+          Location: {oneTeam.city}, {oneTeam.state}
+        </h3>
+      </div>
+    );
+  }
+}
+
+function TeamList() {
+  return (
+    <div>
+      {teamsData.teams.map((team: Team) => (
+        <TeamComponent key={team.tid} {...team} />
+      ))}
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-
-      <p>Hello!</p>
+      <Heading />
+      <TeamList />
     </div>
   );
 }
